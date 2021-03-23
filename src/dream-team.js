3bb493b862062830b115effd9a0a5c77d5d913a1
KrivovDima <krivovdima89@gmail.com>
2021-03-24 00:28:25 +0400

    feat: implement carbon-dating, count-cats, dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const arrStr = members.filter((item) => {
+    return typeof item === 'string';
+  }).map((item) => {
+    let str = item.trim();
+    return str[0].toUpperCase();
+  })
+
+  return arrStr.sort().join('');
+
+ 
 };
